834c7ec968f48b3f7ee2b632bf2e6604f81b0414
Steppa777 <74417086+Steppa777@users.noreply.github.com>
2021-04-12 15:51:56 +0500

    Create Lab18_3.cpp

---
+++ Семестр 2/LAB18/LAB18_3/Lab18_3.cpp
@@ -0,0 +1,21 @@
+#include "money.h" 
+#include <iostream> 
+using namespace std;
+int main() {
+money a;
+money b;
+money c;
+cin >> a;
+cin >> b;
+cout << "============================================" << endl;
+cout << "Первая сумма денег:" << a <<" рублей" << endl;
+cout << "Вторая сумма денег:" << b <<" рублей" << endl;
+cout << "============================================" << endl;
+c = a + b;
+cout << "a= " << a << endl;
+cout << "b= " << b << endl;
+cout << "Сумма(1 и 2) = " << c << endl;
+cout << "============================================" << endl;
+cout << "a > b = " << (a > b) << endl;
+cout << "a < b = " << (a < b) << endl;
+}
